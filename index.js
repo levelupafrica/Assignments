22e0126e0394683fb4ed41e57f0763053dc7e022
Moronke <moronkealiu02@gmail.com>
2019-02-14 17:55:12 +0100

    Create a pull request for a review of my React assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StateComponent from './App';
+import NewStateComponent from './StateComponent'
+
+ 
+ReactDOM.render(
+<div>
+       <StateComponent/>
+       <NewStateComponent/>
+</div>, document.getElementById('root'));
+
